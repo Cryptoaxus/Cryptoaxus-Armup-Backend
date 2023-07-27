0d004d1f52e321ebc9dce33342a8ef89980abf6b
Junaid <134013153+junaid-softaxus@users.noreply.github.com>
2023-07-28 01:51:51 +0500

    CRYP-117: Added Response Examples

---
+++ src/CryptoAxus.Application/Features/NFT/DeleteNftById/Response/NotFoundDeleteNftByIdResponseExample.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoAxus.Application.Features.NFT.DeleteNftById.Response;
+
+public class NotFoundDeleteNftByIdResponseExample : IExamplesProvider<NotFoundDeleteNftByIdResponse>
+{
+    public NotFoundDeleteNftByIdResponse GetExamples()
+    {
+        return new NotFoundDeleteNftByIdResponse("No nft exist against id: 507f191e810c19729de860ea");
+    }
+}
